3338f84f9b88682d9740dd9472ad288438d98d36
Alexander Ananchenko <snvtr@mail.ru>
2018-12-10 17:52:40 +0300

    oops

---
+++ terraform/modules/app/variables.tf
@@ -0,0 +1,12 @@
+variable zone {
+  description = "Region/Zone"
+  default     = "europe-west1-b"
+}
+
+variable public_key_path {
+  description = "Path to my public key"
+}
+
+variable app_disk_image {
+  description = "App disk image"
+}
