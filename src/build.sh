1bdd67f468ddf6a3ae81ce3dee2870f464e61431
Alexander Ananchenko <snvtr@mail.ru>
2019-01-10 08:45:36 +0300

    Logging-1: init

--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-docker build -t snvtr/ui:1.0 ./ui
-docker build -t snvtr/comment:1.0 ./comment
-docker build -t snvtr/post:1.0 ./post-py
+export USER_NAME=snvtr
+export USERNAME=snvtr
 
+docker build -t $USER_NAME/ui:2.1 ./ui
+docker build -t $USER_NAME/comment:2.1 ./comment
+docker build -t $USER_NAME/post:2.1 ./post-py
+
+docker push $USER_NAME/ui
+docker push $USER_NAME/comment
+docker push $USER_NAME/post
